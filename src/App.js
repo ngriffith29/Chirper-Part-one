6bbbe873bab5d3f17dff963875877229fe847f57
Nathan Griffith <ngriffith29@student.egcc.edu>
2018-08-21 11:25:02 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import '.'
+import Nate from './Nate';
+class App extends Component {
+  render() {
+    return (
+      <div>
+       <h1>hello world</h1>
+       <Nate />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Nate.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Nate extends Component {
+  render() {
+    return (
+      <div>
+       <h1>hey yall</h1>
+      </div>
+    );
+  }
+}
+
+export default Nate;
